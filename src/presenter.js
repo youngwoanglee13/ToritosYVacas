446971046bea55e570d7b23e8f4d15f90197a1d8
younglee <young.lee@ucb.edu.bo>
2022-10-08 17:41:54 -0400

    Al ingresar clave valida habilitar boton intentar y deshabilitar boton iniciar

--- src/presenter.js
+++ src/presenter.js
@@ -2,12 +2,13 @@ import TorosYVacas from "./TorosYVacas.js";
 const toro = new TorosYVacas();
 const codigo =document.getElementById("idcodigo");
 const inicioForm = document.querySelector("#inicio-form");
-
+document.getElementById("idintentar").disabled=true;
 inicioForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   if(toro.setClave(codigo.value)=="ok"){
-   
+    document.getElementById("idintentar").disabled=false;
+    document.getElementById("idiniciar").disabled=true;
   }else{
     window.alert(toro.setClave(codigo.value));
   }
